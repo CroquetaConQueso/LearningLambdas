81cc420efa8073d71cb15b5e97ac7f8821dd9e80
CroquetaConQueso <lacio161@gmail.com>
2025-08-02 14:18:51 +0200

    Understanding more

--- l2.java
+++ l2.java
@@ -1,10 +1,11 @@
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class l2{
 
     public static void main(String[] args){
-        List<Product> products = List.of(new Product("Wawa","Cafe",3.12f),new Product("Wiwi","Alcohol",40.2f));
+        /* List<Product> products = List.of(new Product("Wawa","Cafe",3.12f),new Product("Wiwi","Alcohol",40.2f));
         
         ProccessProduct proccessProduct = p -> "Name: " + p.getProductName() + " | Price " +p.getProductPrice();
         
@@ -16,5 +17,20 @@ public class l2{
         String nombresUnidos = nombre.stream().collect(Collectors.joining(", "));
 
         System.out.println(nombresUnidos);
+        */
+
+        Notas notas = new Notas();
+        notas.eliminarSuspensos();
+
+        Product pepito = new Product("Wiwi", "Wawa" , 20.1f);
+
+        Optional<Product> emptyProduct = Optional.empty();
+
+        Product trying = emptyProduct.orElse(new Product("Vacio","wiwi",0));
+
+        System.out.println(emptyProduct);
+        System.out.println(trying.getProductName() + trying.getProductType() + trying.getProductPrice());
+
+
     }
 }
